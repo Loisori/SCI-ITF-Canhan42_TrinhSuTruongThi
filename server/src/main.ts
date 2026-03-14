b0d057b71fa7117060988e561457b6cc99a769ce
loihuynh <121299764+Loisori@users.noreply.github.com>
2026-03-15 01:52:41 +0700

    Update server port from server branch

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,17 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Turn on CORS to Client (3000) can call Server (3001)
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    // methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  // const port = process.env.PORT || 3001;
+  // await app.listen(port);
+  await app.listen(process.env.PORT ?? 3001);
+  console.log(`🚀 Server đang chạy tại: http://localhost:${process.env.PORT ?? 3001}`);
 }
 bootstrap();
