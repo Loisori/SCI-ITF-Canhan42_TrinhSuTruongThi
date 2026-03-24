a73eaa8ac35e353bdd702ac5d37add696c203e57
loihuynh <121299764+Loisori@users.noreply.github.com>
2026-03-24 13:04:20 +0700

    udate enableCors for local

--- server/src/main.ts
+++ server/src/main.ts
@@ -18,16 +18,20 @@ async function bootstrap() {
   // 2. Cấu hình CORS linh hoạt
   // Khi deploy, CORS_ORIGIN sẽ là link Vercel của bạn
   const corsOrigin = process.env.CORS_ORIGIN || 'http://localhost:3000';
+
+  // Tách chuỗi thành mảng nếu có dấu phẩy
+const originList = corsOrigin.split(',').map(item => item.trim());
+
   app.enableCors({
-    origin: corsOrigin,
+    origin: originList, // Truyền mảng các link được phép vào đây
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
 
   // 3. Lắng nghe trên 0.0.0.0 (Bắt buộc cho các Cloud Hosting)
   const port = process.env.PORT || 3001;
-  await app.listen(port, '0.0.0.0'); 
-  
+  await app.listen(port, '0.0.0.0');
+
   logger.log(`🚀 InvestPro Backend is running on: ${await app.getUrl()}`);
   logger.log(`🌍 Accepting requests from: ${corsOrigin}`);
 }
